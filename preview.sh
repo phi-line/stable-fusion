f1f3f1cf7ebc35ca09e6d0358fe5231e71a0b562
kishan <phi.liney@gmail.com>
2023-02-07 15:37:25 -0800

    updated preview.sh

--- preview.sh
+++ preview.sh
@@ -2,4 +2,5 @@
 
 source ./.env
 
-watch -n 300 ffmpeg -y -vcodec png -r $FPS -start_number 0 -an -i $FRAME_PATH/${TIMESTAMP}_%05d.png -vn -i $VIDEO_PATH -pix_fmt yuv420p -crf 17 -preset veryfast -vcodec libx264 -vf fps=$FPS -shortest $FRAME_PATH/preview.mp4
+cat $(find $FRAME_PATH -maxdepth 1 -name "*.png" | sort -V) | ffmpeg -framerate $FPS -i - $FRAME_PATH/preview$FPS.mp4 -v debug
+# ffmpeg -y -vcodec png -r $FPS -start_number 0 -an -i $FRAME_PATH/${PREFIX}%d$SUFFIX.png -vn -i $VIDEO_PATH -pix_fmt yuv420p -crf 17 -preset veryfast -vcodec libx264 -vf fps=$FPS -shortest $FRAME_PATH/preview.mp4
